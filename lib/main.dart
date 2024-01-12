6c6c51c281826369f2d00655610d1d6a1ecc7f92
SandroMaglione <lass.maglio@gmail.com>
2024-01-12 05:35:05 +0100

    grid setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const gridSize = 4;
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +17,35 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      home: Column(
+        children: [
+          Container(
+            height: 600,
+            width: 600,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+            ),
+            child: GridView.count(
+              crossAxisCount: gridSize,
+              children: List.generate(
+                gridSize * gridSize,
+                (index) => Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Item $index',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
