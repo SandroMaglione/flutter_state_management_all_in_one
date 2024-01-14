67e12196a7008682fc161ed88bdda945d5aebeba
SandroMaglione <lass.maglio@gmail.com>
2024-01-14 12:30:09 +0100

    extract grid layout

--- lib/bloc/grid.dart
+++ lib/bloc/grid.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_state_management_all_in_one/bloc/blocs/board/board_bloc.dart';
 import 'package:flutter_state_management_all_in_one/bloc/blocs/gesture/gesture_bloc.dart';
-import 'package:flutter_state_management_all_in_one/cell_card.dart';
+import 'package:flutter_state_management_all_in_one/grid_layout.dart';
 import 'package:flutter_state_management_all_in_one/grid_settings.dart';
 
 class Grid extends StatelessWidget {
@@ -14,43 +14,22 @@ class Grid extends StatelessWidget {
     final gestureBlocState = context.watch<GestureBloc>().state;
     final gridSettings = context.watch<GridSettings>();
     final boardBloc = context.watch<BoardBloc>();
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          "Found ${boardBloc.state.foundWords.length} words | ${boardBloc.state.points} points",
-        ),
-        GestureDetector(
-          onPanStart: (details) => gestureBloc.add(OnPanStart(details)),
-          onPanUpdate: (details) => gestureBloc.add(OnPanUpdate(details)),
-          onPanEnd: (details) {
-            boardBloc
-                .add(SearchWord(context.read<GestureBloc>().state.indexes));
-            gestureBloc.add(OnPanEnd(details));
-          },
-          child: Container(
-            width: gridSettings.gridDimension,
-            height: gridSettings.gridDimension,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-            ),
-            child: GridView.count(
-              physics: const NeverScrollableScrollPhysics(),
-              padding: const EdgeInsets.all(0),
-              crossAxisCount: gridSettings.gridSize,
-              children: boardBloc.state.cells
-                  .map(
-                    (gridCell) => CellCard(
-                      gridCell: gridCell,
-                      isSelected: gestureBlocState.isSelected(gridCell.index),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ),
-        ),
-      ],
+    return GridLayout(
+      gridSettings: gridSettings,
+      foundWords: boardBloc.state.foundWords.length,
+      points: boardBloc.state.points,
+      cells: boardBloc.state.cells,
+      isSelected: gestureBlocState.isSelected,
+      onPanStart: (details) {
+        gestureBloc.add(OnPanStart(details));
+      },
+      onPanUpdate: (details) {
+        gestureBloc.add(OnPanUpdate(details));
+      },
+      onPanEnd: (details) {
+        boardBloc.add(SearchWord(context.read<GestureBloc>().state.indexes));
+        gestureBloc.add(OnPanEnd(details));
+      },
     );
   }
 }
---
+++ lib/grid_layout.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_management_all_in_one/cell_card.dart';
+import 'package:flutter_state_management_all_in_one/grid_settings.dart';
+import 'package:flutter_state_management_all_in_one/typedefs.dart';
+
+class GridLayout extends StatelessWidget {
+  final int foundWords;
+  final int points;
+  final GridSettings gridSettings;
+  final List<GridCell> cells;
+  final void Function(DragStartDetails details) onPanStart;
+  final void Function(DragUpdateDetails details) onPanUpdate;
+  final void Function(DragEndDetails details) onPanEnd;
+  final bool Function(GridIndex index) isSelected;
+
+  const GridLayout({
+    required this.gridSettings,
+    required this.foundWords,
+    required this.points,
+    required this.cells,
+    required this.onPanEnd,
+    required this.onPanStart,
+    required this.onPanUpdate,
+    required this.isSelected,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          "Found $foundWords words | $points points",
+        ),
+        GestureDetector(
+          onPanStart: onPanStart,
+          onPanUpdate: onPanUpdate,
+          onPanEnd: onPanEnd,
+          child: Container(
+            width: gridSettings.gridDimension,
+            height: gridSettings.gridDimension,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+            ),
+            child: GridView.count(
+              physics: const NeverScrollableScrollPhysics(),
+              padding: const EdgeInsets.all(0),
+              crossAxisCount: gridSettings.gridSize,
+              children: cells
+                  .map(
+                    (gridCell) => CellCard(
+                      gridCell: gridCell,
+                      isSelected: isSelected(gridCell.index),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
